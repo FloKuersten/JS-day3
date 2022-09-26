90e96d6a743fd243b853fda6cae0e0a481d38127
Florian Kürsten <f.kuersten@hotmail.com>
2022-09-26 15:52:55 +0200

    -

---
+++ TM Flo/Basic/Exe2/script.js
@@ -0,0 +1,8 @@
+let tempNow = (Math.random()*31) - 5
+console-console.log(tempNow);
+
+if ( 11 <= tempNow ) {
+    document.write("Today it is hot outside.")
+} else {
+        document.write("Today it is cold outside.")
+    }
